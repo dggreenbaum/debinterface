3c18d9cf5dc62bb3115f077e793938a420ac04cd
Nathan Mustaki <feydaykyn@gmail.com>
2014-09-24 11:03:07 +0200

    one function to control network service, Interfaces._adapters is now a property

--- interfaces.py
+++ interfaces.py
@@ -6,8 +6,17 @@ import tools
 
 
 class Interfaces:
-    def __init__(self):
-        self.adapters = self.parseInterfaces()
+    def __init__(self, read_interface=True):
+        ''' By default read interface file on init '''
+
+        if read_interface:
+            self._adapters = self.parseInterfaces()
+        if not self._adapters:
+            self._adapters = []
+
+    @property
+    def adapters(self):
+        return self._adapters
 
     def parseInterfaces(self):
         ''' Read /etc/network/interfaces.
@@ -18,25 +27,20 @@ class Interfaces:
     def addAdapter(self, options, index=None):
         ''' Insert a networkAdapter before the given index or at the end of the list. '''
         if index is None:
-            self.adapters.insert(index, NetworkAdapter(options))
+            self._adapters.insert(index, NetworkAdapter(options))
         else:
-            self.adapters.append(NetworkAdapter(options))
+            self._adapters.append(NetworkAdapter(options))
 
     def removeAdapter(self, index):
         ''' Remove the adapter at the given index. '''
-        self.adapters.pop(index)
+        self._adapters.pop(index)
 
     def writeInterfaces(self):
-        return InterfacesWriter(self.adapters).write_interfaces()
+        return InterfacesWriter(self._adapters).write_interfaces()
 
-    def stopNetworkService(self):
+    def controlNetworkService(self, action):
         ''' return True/False, command output '''
-        return tools.safe_subprocess(["/etc/init.d/networking", "stop"])
 
-    def startNetworkService(self):
-        ''' return True/False, command output '''
-        return tools.safe_subprocess(["/etc/init.d/networking", "start"])
-
-    def restartNetworkService(self):
-        ''' return True/False, command output '''
-        return tools.safe_subprocess(["/etc/init.d/networking", "restart"])
+        if action not in ["start", "stop", "restart"]:
+            return False, "Invalid action"
+        return tools.safe_subprocess(["/etc/init.d/networking", action])
