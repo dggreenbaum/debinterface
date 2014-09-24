2339ed0b1375d7473f9a3a0429bcf06e7e30c601
Nathan Mustaki <feydaykyn@gmail.com>
2014-09-24 12:52:40 +0200

    rename constants.py into defaults.py + INTERFACES to INTERFACES_PATH

--- constants.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/python
-
-# Define file locations and other constants in a centralized location.
-
-INTERFACES = '/tmp/interfaces' #'/etc/network/interfaces'
-BACKUP = '/etc/network/interfaces.old'
---
+++ defaults.py
@@ -0,0 +1,4 @@
+# Define file locations and other constants in a centralized location.
+
+INTERFACES_PATH = './interfaces'  # '/etc/network/interfaces'
+BACKUP_PATH = '/tmp/interfaces.old'  # '/etc/network/interfaces.old'
