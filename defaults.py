253d91877e4b871f36e1642dabf73b344459943d
Nathan Mustaki <feydaykyn@gmail.com>
2014-10-02 10:44:13 +0200

    removed defaults.py, no need

--- defaults.py
+++
@@ -1,4 +0,0 @@
-# Define file locations and other constants in a centralized location.
-
-INTERFACES_PATH = '/etc/network/interfaces'
-BACKUP_PATH = '/etc/network/interfaces.bak'  # '/etc/network/interfaces.old'
